a9075270f2f968dbc4e789ccd106d347a28f6f94
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-07-17 06:47:34 +0200

    Refaktoruj herní smyčku a vylepši formátování výstupu ve hře hádání čísel.

--- 100 DAYS of Code/Day 12/number guess project/main.py
+++ 100 DAYS of Code/Day 12/number guess project/main.py
@@ -9,7 +9,8 @@ def show_welcome_screen():
 
 def set_secret_number(num_range):
     rnd = random.randrange(num_range.start, num_range.stop)
-    print(f"I'm thinking of a number between {num_range.start} and {num_range.stop}.")
+    print(f"I'm thinking of a number between {num_range.start}"
+          f"and {num_range.stop}.")
     return rnd
 
 
@@ -19,30 +20,42 @@ def set_difficulty():
         return 10
     elif diff == 'hard':
         return 5
-    
+
+
+def game_loop(attempts):
+
+    game_over = False
+
+    while attempts > 0 and game_over is False:
+        print(f"You have {attempts} attempts remaining to guess the number.")
+        print(f"#secret_number = {secret_number}")
+        guess = int(input("Make a guess: "))
+
+        if guess > secret_number:
+            print("Too hight.")
+        elif guess < secret_number:
+            print("Too low.")
+        else:
+            game_over = True
+        attempts -= 1
+
+    if attempts > 0:
+        return True
+    else:
+        return False
+
 
 secret_number = None
 attempts = None
-game_over = False
 
 
 show_welcome_screen()
 secret_number = set_secret_number(range(0, 100))
 attempts = set_difficulty()
 
-print(f"You have {attempts} attempts remaining to guess the number.")
+win = game_loop(attempts)
 
-while attempts > 0 and game_over == False:
-    print(f"#secret_number = {secret_number}")
-    guess = int(input("Make a guess: "))
-
-    if guess > secret_number:
-        print("Too hight.")
-    elif guess < secret_number:
-        print("Too low.")
-    else:
-        print(f"You got it! The answer was {secret_number}.")
-        game_over = True
-    attempts -= 1
-
-
+if win:
+    print(f"You got it! The answer was {secret_number}.")
+else:
+    print("You've run out of guesses. GAME OVER.")
