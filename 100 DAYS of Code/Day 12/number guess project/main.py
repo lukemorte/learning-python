0abe8cad8d307f10ca6273b60af916a01decb07d
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-07-17 07:22:37 +0200

    Refaktoring herní logiky pro lepší čitelnost a udržovatelnost.

--- 100 DAYS of Code/Day 12/number guess project/main.py
+++ 100 DAYS of Code/Day 12/number guess project/main.py
@@ -2,33 +2,36 @@ import random
 from art import logo
 
 
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
+
 def show_welcome_screen():
     print(logo)
     print("Welcome to the Number Guessing Game!")
 
 
-def set_secret_number(num_range):
-    rnd = random.randrange(num_range.start, num_range.stop)
+def set_secret_number(num_range):    
+    rnd = random.randint(num_range.start, num_range.stop)
     print(f"I'm thinking of a number between {num_range.start}"
           f"and {num_range.stop}.")
     return rnd
 
 
 def set_difficulty():
-    diff = input("Choose a difficulty. Type 'easy' or 'hard': ")
-    if diff == 'easy':
-        return 10
-    elif diff == 'hard':
-        return 5
+    level = input("Choose a difficulty. Type 'easy' or 'hard': ")
+    if level == 'easy':
+        return EASY_LEVEL_TURNS
+    elif level == 'hard':
+        return HARD_LEVEL_TURNS
 
 
-def game_loop(attempts):
+def game_loop(attempts, secret_number):
 
     game_over = False
 
     while attempts > 0 and game_over is False:
         print(f"You have {attempts} attempts remaining to guess the number.")
-        print(f"#secret_number = {secret_number}")
         guess = int(input("Make a guess: "))
 
         if guess > secret_number:
@@ -45,17 +48,22 @@ def game_loop(attempts):
         return False
 
 
-secret_number = None
-attempts = None
+def game():
+    secret_number = None
+    attempts = None
+
+    show_welcome_screen()
+    secret_number = set_secret_number(range(0, 100))
+    attempts = set_difficulty()
+
+    win = game_loop(attempts, secret_number)
+
+    if win:
+        print(f"You got it! The answer was {secret_number}.")
+    else:
+        print("You've run out of guesses. GAME OVER.")
 
 
-show_welcome_screen()
-secret_number = set_secret_number(range(0, 100))
-attempts = set_difficulty()
+# program code
 
-win = game_loop(attempts)
-
-if win:
-    print(f"You got it! The answer was {secret_number}.")
-else:
-    print("You've run out of guesses. GAME OVER.")
+game()
