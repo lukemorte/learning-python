0f5015d21eaa41972ded4b446849efef13cd897e
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-07-17 17:25:20 +0200

    rozpracovaný postup compare

--- 100 DAYS of Code/Day 14 - Higher Lower Game/main.py
+++ 100 DAYS of Code/Day 14 - Higher Lower Game/main.py
@@ -3,6 +3,9 @@ from people_data import people
 import art
 
 
+# functions
+
+
 def test():
     for man in people:
         print(
@@ -20,24 +23,43 @@ def get_random_person():
 
 
 def show_question(persons):
+    clr()
+    print(art.logo)
+
     print(f"Compare A: {persons[0]["name"]}, "
           f"{persons[0]["profession"]}, "
           f"from {persons[0]["country"]}.")
 
     print(art.logo_vs)
 
-    print(f"Compare B: {persons[1]['name']}, "
+    print(f"Against B: {persons[1]['name']}, "
           f"{persons[1]['profession']}, "
           f"from {persons[1]['country']}.")
 
 
-clr()
-# print(art.logo)
-# print("")
-# print(art.logo_vs)
+def get_answer():
+    answer = ""
+    print("")
+    while answer.lower() != 'a' and answer.lower() != 'b':
+        answer = input("Who has more followers? Type 'A' or 'B': ")
+    return answer.lower()
+
+# vars
+
+
+score = 0
+
+
+# code
 
 
 two_persons = [get_random_person(), get_random_person()]
-
-
 show_question(two_persons)
+
+answer = get_answer()
+
+if (answer == 'a' and two_persons[0]['followers'] > two_persons[1]['followers']) or (answer == 'b' and two_persons[1]['followers'] > two_persons[0]['followers']):
+    score += 1
+    two_persons[0] = two_persons[1]
+    two_persons[1] = get_random_person()
+    show_question(two_persons)
