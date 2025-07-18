4f03c680ffe6e46372845676c7cfc192f949dab0
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-07-18 07:12:56 +0200

    Refactor celé hry Higher Lower Game, hra hotová

--- 100 DAYS of Code/Day 14 - Higher Lower Game/main.py
+++ 100 DAYS of Code/Day 14 - Higher Lower Game/main.py
@@ -3,15 +3,13 @@ from people_data import people
 import art
 
 
+# const
+
+
+TESTING = True
+
+
 # functions
-
-
-def test():
-    for man in people:
-        print(
-            f"{man['name']}, {man['profession']}, from {man['country']} ... "
-            f"{man['followers']}"
-        )
 
 
 def clr():
@@ -22,10 +20,18 @@ def get_random_person():
     return random.choice(people)
 
 
-def show_question(persons):
-    clr()
-    print(art.logo)
+def fill_second_person(arr):
+    p2 = get_random_person()
+    while p2 == arr[0]:
+        p2 = get_random_person()
 
+    if len(arr) > 1:
+        arr[1] = p2
+    else:
+        arr.append(p2)    
+
+
+def show_question(persons):
     print(f"Compare A: {persons[0]["name"]}, "
           f"{persons[0]["profession"]}, "
           f"from {persons[0]["country"]}.")
@@ -36,6 +42,10 @@ def show_question(persons):
           f"{persons[1]['profession']}, "
           f"from {persons[1]['country']}.")
 
+    if TESTING:
+        test_winner = max(persons, key=lambda n: n['followers'])
+        print(f"(winner = '{test_winner["name"]}')")
+
 
 def get_answer():
     answer = ""
@@ -44,22 +54,38 @@ def get_answer():
         answer = input("Who has more followers? Type 'A' or 'B': ")
     return answer.lower()
 
-# vars
 
+def play_game():
+    score = 0
+    game_over = False
 
-score = 0
+    two_persons = [get_random_person()]
+    fill_second_person(two_persons)
+
+    while game_over is False:
+        clr()
+        print(art.logo)
+
+        if score > 0:
+            print(f"You're right! Current score: {score}.")
+
+        show_question(two_persons)
+        answer = get_answer()
+
+        if (
+            answer == 'a' and two_persons[0]['followers'] > two_persons[1]['followers']) or (
+            answer == 'b' and two_persons[1]['followers'] > two_persons[0]['followers']
+        ):
+            score += 1
+            two_persons[0] = two_persons[1]
+            fill_second_person(two_persons)
+
+        else:
+            game_over = True
+            print(f"\nGAME OVER! You ended with score {score}. Congratulations.")
 
 
 # code
 
 
-two_persons = [get_random_person(), get_random_person()]
-show_question(two_persons)
-
-answer = get_answer()
-
-if (answer == 'a' and two_persons[0]['followers'] > two_persons[1]['followers']) or (answer == 'b' and two_persons[1]['followers'] > two_persons[0]['followers']):
-    score += 1
-    two_persons[0] = two_persons[1]
-    two_persons[1] = get_random_person()
-    show_question(two_persons)
+play_game()
