a6c0368cb13ba07a0bd9e0e3f5de1cf155f47e73
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-07-20 09:08:47 +0200

    Refactor main.py to comment out turtle graphics code and add PrettyTable implementation for displaying Pokémon data.

--- 100 DAYS of Code/Day 16 - OOP/main.py
+++ 100 DAYS of Code/Day 16 - OOP/main.py
@@ -1,10 +1,19 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.color("red")
-timmy.shapesize(2)
-timmy.forward(100,)
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.shapesize(2)
+# timmy.forward(100,)
 
-my_screen = Screen()
-my_screen.exitonclick()
+# my_screen = Screen()
+# my_screen.exitonclick()
+
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("Pokemon name", ["Pikachu", "Squirtle", "Charmander"], "l")
+table.add_column("Type", ["Electric", "Water", "Fire"], "l")
+
+print(table)
