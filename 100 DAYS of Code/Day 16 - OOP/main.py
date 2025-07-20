6b055db872bc459a9bdc3d55bbb401c76d86e60b
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-07-20 06:55:14 +0200

    16 -OOP,  Přidána počáteční implementaci turtle grafiky v main.py

---
+++ 100 DAYS of Code/Day 16 - OOP/main.py
@@ -0,0 +1,10 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+timmy.shapesize(2)
+timmy.forward(100,)
+
+my_screen = Screen()
+my_screen.exitonclick()
