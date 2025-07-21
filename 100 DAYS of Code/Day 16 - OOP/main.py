c3c765bfa17325e1d55baae77c568b24135c80e6
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-07-21 06:01:41 +0200

    quiz hotov

--- 100 DAYS of Code/Day 16 - OOP/main.py
+++ 100 DAYS of Code/Day 16 - OOP/main.py
@@ -13,7 +13,12 @@
 from prettytable import PrettyTable
 
 table = PrettyTable()
-table.add_column("Pokemon name", ["Pikachu", "Squirtle", "Charmander"], "l")
-table.add_column("Type", ["Electric", "Water", "Fire"], "l")
+table.add_column("Pokemon name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+
+print(table.align)
+table.align = "l"
+print(table.align)
 
 print(table)
--- 100 DAYS of Code/Day 7/main.py
+++ 100 DAYS of Code/Day 7/main.py
@@ -3,10 +3,6 @@ import random
 
 word_list = ["aadwark", "baboon", "camel"]
 
-# TODO-1 - randomly choose a word from the word_list and assign it to a variable called chosen_word.
-# TODO-2 - ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-# TODO-3 - check if the letter the user guessed (guess) is one of the letters in the chosen_word. Write "Right" if it is, write "Wrong" if it's not.
-
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
